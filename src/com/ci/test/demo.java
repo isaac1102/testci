ec685339071bd99bf0d62f2888b062d364d2b2fa
isaac1102 <37571052+isaac1102@users.noreply.github.com>
2021-06-29 22:36:42 +0900

    Update demo.java

--- src/com/ci/test/demo.java
+++ src/com/ci/test/demo.java
@@ -3,10 +3,5 @@ package com.ci.test;
 public class demo {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		System.out.println("update 1");
-		System.out.println("update 2");
-		System.out.println("update 3");
-		System.out.println("update 4");
-		System.out.println("update 5");
 	}
 }
