cdcb41f582f108b6b6a3ef566de3084e05053e30
isaac1102 <37571052+isaac1102@users.noreply.github.com>
2021-06-29 22:44:07 +0900

    Update demo.java

--- src/com/ci/test/demo.java
+++ src/com/ci/test/demo.java
@@ -3,6 +3,6 @@ package com.ci.test;
 public class demo {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		System.out.println("Hello World!2");
+		System.out.println("Hello World!3");
 	}
 }
